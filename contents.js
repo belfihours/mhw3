737bf0e9abdcd803131a4784c6c811fe80c095d0
belfihours <71651454+belfihours@users.noreply.github.com>
2021-04-22 15:37:31 +0200

    Add files via upload

---
+++ contents.js
@@ -0,0 +1,121 @@
+const pane=[
+    {
+        titolo: 'Pane Bianco', 
+        descrizione: 'Pane semplice con Farina 00', 
+        immagine: 'https://www.gustissimo.it/articoli/scuola-di-cucina/impasti-e-pastelle/mafalda-siciliana.jpg'
+    },
+    {
+        titolo: 'Pane di Semola', 
+        descrizione: 'Pane semplice con Farina di semola', 
+        immagine: 'https://blog.cookaround.com/veronic/wp-content/uploads/2020/03/pizap.com14649848128272.jpg'
+    },
+    {
+        titolo: 'Pane di Casa', 
+        descrizione: 'Pane senza lievito aggiunto', 
+        immagine: 'https://uploads.nonsprecare.it/wp-content/uploads/2014/11/ricetta-pane-fatto-in-casa.jpg'
+    },
+    {
+        titolo: 'Pane di Timilia', 
+        descrizione: 'Pane con farina speciale di Timilia', 
+        immagine: 'https://files.synapp.it/1340202/foto/prodotti/B/1599302237454_dsc_6006m_B.jpg'
+    },
+    {
+        titolo: 'Pane ai 5 Cereali', 
+        descrizione: 'Pane con farina ai 5 Cereali', 
+        immagine: 'http://2.bp.blogspot.com/-4T0v1eehWBw/UUA7JxmbtoI/AAAAAAAAALY/rVHUzP5wV-U/s640/CIMG2907.JPG'
+    },
+    {
+        titolo: 'Morbidoni', 
+        descrizione: 'Pane adatto per spuntini e Hamburgers', 
+        immagine: 'https://tuttofabrodoincucina.it/wp-content/uploads/2020/09/PANINI-MORBIDONI-AL-LATTE-3-1-1200x900.jpg'
+    }
+]
+
+const pizza=[
+    {
+        titolo: 'Margherita', 
+        descrizione: 'pomodoro, mozzarella, olio e origano', 
+        immagine: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/500xNxpizza-margherita.jpg.pagespeed.ic.azeCEpJjc5.jpg'
+    },
+    {
+        titolo: 'Capricciosa', 
+        descrizione: 'pomodoro, mozzarella, prosciutto cotto, funghi, olive e carciofini', 
+        immagine: 'https://i2.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2017/12/3240_Pizza.jpg?resize=895%2C616&ssl=1'
+    },
+    {
+        titolo: 'Patatina', 
+        descrizione: 'pomodoro, mozzarella, patatine fritte e olio', 
+        immagine: 'https://www.dolcidee.it/media/uploads/recipe/5ede46927ca62_tmp6d9b772b4286436abd426b9e1c3769ba.jpg'
+    },
+    {
+        titolo: 'Piccante', 
+        descrizione: 'pomodoro, mozzarella, salame piccante, cipolla, olive e olio', 
+        immagine: 'https://i2.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2016/07/3019_Pizza.jpg?resize=895%2C616&ssl=1'
+    },
+]
+const biscotti=[
+    {
+        titolo: 'Biscotti al latte', 
+        descrizione: 'Biscotti leggeri per chi vuole stare in forma', 
+        immagine: 'https://i.pinimg.com/564x/3e/76/08/3e76080e2613dfb0f962c4ea5698fccc.jpg'
+    },
+    {
+        titolo: 'Occhi di bue', 
+        descrizione: 'Biscotti semplici con nutella o marmellata', 
+        immagine: 'https://www.giallozafferano.it/images/23-2313/Occhio-di-bue_650x433_wm.jpg'
+    },
+    {
+        titolo: 'Meringhe', 
+        descrizione: "Solo zucchero, una tira l'altra", 
+        immagine: 'https://dolciveloci.it/wp-content/uploads/2019/11/SH_meringhe_colorate.jpg'
+    },
+    {
+        titolo: 'Biscotti della monaca', 
+        descrizione: 'Biscotti classici e tradizionali', 
+        immagine: 'https://www.quotidianodiragusa.com/cache/2019/02/biscotti-monaca-ricetta-siciliana-410_900x600.jpg'
+    },
+]
+const bevande=[
+    {
+        titolo: 'Coca Cola', 
+        descrizione: 'La classica bevanda amata da tutti', 
+        immagine: 'https://www.coca-colaitalia.it/content/dam/one/it/it/homepage/carousel/desktop/CocaCola-KeelClip-1600x900_new.jpg'
+    },
+    {
+        titolo: 'Monster', 
+        descrizione: 'Bibita energetica in vari gusti', 
+        immagine: 'https://perfectbody360.it/site/wp-content/uploads/2019/03/monster-energy.jpg'
+    },
+    {
+        titolo: 'Birra', 
+        descrizione: 'Birra di varie marche', 
+        immagine: 'https://www.beverfood.com/wp-content/uploads/2020/03/iri-white-paper-birra-2020-pinta-boccale.jpg'
+    },
+    {
+        titolo: 'Tè', 
+        descrizione: 'Bibita rifrescante, pesca o limone', 
+        immagine: 'https://media.area-gourmet.com/product/te-frio-melocoton-hibiscus-400ml-fuze-tea-800x800.jpeg'
+    },
+]
+const ingredienti=[
+    {
+        titolo: 'Farina 00', 
+        descrizione: 'Farina semplice', 
+        immagine: 'https://www.greenme.it/wp-content/uploads/2013/05/farina-bianca.jpg'
+    },
+    {
+        titolo: 'Farina di Semola', 
+        descrizione: 'Farina di semola per panini come schiacciatelle', 
+        immagine: 'https://file.cure-naturali.it/site/image/content/23056.jpg?format=jpg'
+    },
+    {
+        titolo: 'Uova', 
+        descrizione: 'Uova fresche', 
+        immagine: 'https://static.ohga.it/wp-content/uploads/sites/24/2019/10/uova-proprieta%CC%80.jpg'
+    },
+    {
+        titolo: 'Lievito', 
+        descrizione: 'Lievito per impasti',
+        immagine: 'https://www.cicalia.com/it/img/imgproducts/370/l_370.jpg'
+    },
+]
---
+++ script.js
@@ -0,0 +1,325 @@
+function aggiungiContenuto(elemento, sezione){
+    const container= document.createElement('div');
+    const container2= document.createElement('div');
+    container.classList.add('generale');
+    container2.classList.add('container2')
+    const immagine=document.createElement('img');
+    immagine.src=elemento.immagine;
+    const titolo=document.createElement('h2');
+    titolo.textContent=elemento.titolo;
+    const text=document.createElement('h4');
+    text.textContent=('Mostra Dettagli');
+    text.classList.add('mdescrizione');
+    const descrizione=document.createElement('span');
+    descrizione.textContent=elemento.descrizione;
+    descrizione.classList.add('hidden');
+    descrizione.classList.add('descrizione');
+    immagine.classList.add('cibo');
+    const bm=document.createElement('img');
+    bm.src="img/addbookmark.png";
+    bm.classList.add('icon');
+    container.appendChild(titolo);
+    container2.appendChild(text);
+    container.appendChild(immagine);
+    container.appendChild(container2);
+    container2.appendChild(bm);
+    container.appendChild(descrizione);
+    sezione.appendChild(container);
+}
+
+function mostraDescrizione(event){
+    const oggetto= event.currentTarget.parentElement.parentElement;
+    if(oggetto.querySelector('span').classList.contains('show')){
+        oggetto.querySelector('span').classList.add('hidden');
+        oggetto.querySelector('span').classList.remove('show');
+    }
+    else{
+        oggetto.querySelector('span').classList.remove('hidden');
+        oggetto.querySelector('span').classList.add('show');
+    }
+    
+} 
+
+function aggiungiPreferiti(event){
+    const variabile=event.currentTarget.parentElement.parentElement;
+    if(!variabile.classList.contains('bm')){
+    	const sezione=document.querySelector('.preferiti');
+        const container= document.createElement('div');
+        const container2= document.createElement('div');
+        container.classList.add('divpref');
+        container2.classList.add('container2');
+        const immagine=document.createElement('img');
+        immagine.src=variabile.querySelector('img').src;
+        const titolo=document.createElement('h2');
+        titolo.textContent=variabile.querySelector('h2').textContent;
+        const text=document.createElement('h4');
+        text.textContent=('Mostra Dettagli');
+        text.classList.add('mdescrizione');
+        const descrizione=document.createElement('span');
+        descrizione.textContent=variabile.querySelector('span').textContent;
+        descrizione.classList.add('hidden');
+        descrizione.classList.add('descrizione');
+        immagine.classList.add('cibo');
+        const bm=document.createElement('img');
+        bm.src="img/removebookmark.png";
+        bm.classList.add('iconp');
+        container.appendChild(titolo);
+        container2.appendChild(text);
+        container.appendChild(immagine);
+        container.appendChild(container2);
+        container2.appendChild(bm);
+        container.appendChild(descrizione);
+        sezione.appendChild(container);
+        sezione.classList.remove('hidden');
+        variabile.classList.add('bm');
+        text.addEventListener('click', mostraDescrizione);
+        
+        bm.addEventListener('click', rimuoviPreferiti);
+        
+    }
+
+}
+
+function rimuoviPreferiti(event){
+    const sezione=document.querySelector('.preferiti');
+    const elemento=event.currentTarget.parentElement.parentElement;
+    const preferiti=document.querySelectorAll('.preferiti .divpref');
+    const bookmark=document.querySelectorAll('.bm');
+    for(let elementi of bookmark){
+        if(elementi.firstChild.textContent===elemento.firstChild.textContent){
+            elementi.classList.remove('bm');   
+        }
+    }
+    //ha lo stesso effetto anche se metto hidden invece di eliminare
+    elemento.remove();
+    if(!sezione.hasChildNodes()){
+        sezione.classList.add('hidden');
+    }
+    
+}
+
+function caricaContenuto(event){
+    
+    let oggetto=null;
+    let giacliccato=0;
+    if(document.querySelector('div.corrente').classList.contains('hidden')){
+        giacliccato=1;
+    } else{
+        giacliccato=0;
+    }
+    document.querySelector('div.corrente').classList.remove('hidden');
+    document.querySelector('.search').classList.remove('hidden');
+    if(event.currentTarget===ultimoelemento && giacliccato===0){
+        document.querySelector('div.corrente').classList.add('hidden');
+        document.querySelector('.search').classList.add('hidden');
+    }
+
+    if(event.currentTarget===document.querySelector('#Pane')){
+        oggetto=pane;
+        
+    }
+    else if(event.currentTarget===document.querySelector('#Pizza')){
+        oggetto=pizza;
+        
+    }
+    else if(event.currentTarget===document.querySelector('#Biscotti')){
+        oggetto=biscotti;
+        
+    }
+    else if(event.currentTarget===document.querySelector('#Bevande')){
+        oggetto=bevande;
+        
+    }
+    else if(event.currentTarget===document.querySelector('#Ingredienti')){
+        oggetto=ingredienti;
+        
+    }
+    ultimasezione=oggetto;
+
+    while(document.querySelector('div.corrente').firstChild){
+            document.querySelector('div.corrente').removeChild(document.querySelector('div.corrente').firstChild);
+    }
+    ultimoelemento=event.currentTarget;
+    
+    for(let elemento of oggetto){
+        aggiungiContenuto(elemento, document.querySelector('div.corrente'));
+    }
+
+
+    const mostra= document.querySelectorAll('.mdescrizione');
+        for(let button of mostra){
+    button.addEventListener('click', mostraDescrizione);
+    }
+
+    const preferiti= document.querySelectorAll('.icon');
+        for(let button of preferiti){
+    button.addEventListener('click', aggiungiPreferiti);
+    }
+
+
+}
+
+
+function avviaricerca(){
+    
+    const oggetti=document.querySelectorAll('.corrente div.generale');
+    const sezione=document.querySelector('corrente');
+
+    //ricerca locale solo nella scheda selezionata
+    /*if(ricerca.value!==null){
+        for(let oggetto of oggetti){  
+            if(oggetto.firstChild.textContent.toLocaleLowerCase().indexOf(ricerca.value)==-1){  
+                oggetto.classList.add('hidden');
+            }else{
+                oggetto.classList.remove('hidden');
+            }
+        }
+    }*/
+    arrayricerca.splice(0,arrayricerca.length);
+    while(document.querySelector('div.corrente').firstChild){
+        document.querySelector('div.corrente').removeChild(document.querySelector('div.corrente').firstChild);
+    }
+    if(ricerca.value!==""){
+        for(let oggetto of pane){
+            
+            if(oggetto.titolo.toLocaleLowerCase().indexOf(ricerca.value.toLocaleLowerCase())!==-1){
+                arrayricerca.push(oggetto);
+            }
+            
+        }
+        for(let oggetto of pizza){
+            if(oggetto.titolo.toLocaleLowerCase().indexOf(ricerca.value.toLocaleLowerCase())!==-1){
+                arrayricerca.push(oggetto);
+            }
+            
+        }
+        for(let oggetto of biscotti){
+            if(oggetto.titolo.toLocaleLowerCase().indexOf(ricerca.value.toLocaleLowerCase())!==-1){
+                arrayricerca.push(oggetto);
+            }
+            
+        }
+        for(let oggetto of bevande){
+            if(oggetto.titolo.toLocaleLowerCase().indexOf(ricerca.value.toLocaleLowerCase())!==-1){
+                arrayricerca.push(oggetto);
+            }
+            
+        }
+        for(let oggetto of ingredienti){
+            if(oggetto.titolo.toLocaleLowerCase().indexOf(ricerca.value.toLocaleLowerCase())!==-1){
+                arrayricerca.push(oggetto);
+            }     
+        }
+        
+        if(ricerca.length!==0){
+            for(let div of arrayricerca){
+                aggiungiContenuto(div,document.querySelector('div.corrente') );
+
+            }
+            
+        }  
+        const mostra= document.querySelectorAll('.mdescrizione');
+        for(let button of mostra){
+            button.addEventListener('click', mostraDescrizione);
+        }
+
+        const preferiti= document.querySelectorAll('.icon');
+        for(let button of preferiti){
+            button.addEventListener('click', aggiungiPreferiti);
+        } 
+    }
+    else{
+        console.log('ciao');
+        while(document.querySelector('div.corrente').firstChild){
+            document.querySelector('div.corrente').removeChild(document.querySelector('div.corrente').firstChild);
+        }
+    
+        for(let elemento of ultimasezione){
+            aggiungiContenuto(elemento, document.querySelector('div.corrente'));
+        }
+
+        const mostra= document.querySelectorAll('.mdescrizione');
+        for(let button of mostra){
+            button.addEventListener('click', mostraDescrizione);
+        }
+
+        const preferiti= document.querySelectorAll('.icon');
+        for(let button of preferiti){
+            button.addEventListener('click', aggiungiPreferiti);
+        }
+    }
+    
+
+}
+
+function initMap() {
+    const myLatLng = { lat: 37.51484330337947, lng: 15.087038187240512};
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 19,
+      center: myLatLng,
+    });
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+    });
+  }
+
+const foodApi="https://foodish-api.herokuapp.com/";
+
+function onResponse(response){
+    return response.json();
+}
+
+function funzione(json){
+    oggetto.src=json.image;
+}
+
+function cambiaImg(event){
+    oggetto=event.currentTarget;
+    console.log(oggetto);
+    if(oggetto.id=="pane"){
+        fetch(foodApi + "api/images/burger/").then(onResponse).then(funzione);
+        console.log('burger');
+    }
+    else if(oggetto.id=="pizza"){
+        fetch(foodApi + "api/images/pizza/").then(onResponse).then(funzione);
+    }
+    else if(oggetto.id=="biscotti"){
+        fetch(foodApi + "api/images/dessert/").then(onResponse).then(funzione);
+    }
+    else if(oggetto.id=="tavolacalda"){
+        fetch(foodApi + "api/images/samosa/").then(onResponse).then(funzione);
+    }
+    
+}
+
+let oggetto;
+let ultimasezione=null;
+const arrayricerca=[];
+let map;
+let ultimoelemento=null;
+const bPane=document.querySelector('#Pane');
+const bPizza=document.querySelector('#Pizza');
+const bBiscotti=document.querySelector('#Biscotti');
+const bBevande=document.querySelector('#Bevande');
+const bIngredienti=document.querySelector('#Ingredienti');
+const ricerca=document.querySelector('.search input');
+const img1=document.querySelector('.photogallery #pane');
+const img2=document.querySelector('.photogallery #pizza');
+const img3=document.querySelector('.photogallery #biscotti');
+const img5=document.querySelector('.photogallery #tavolacalda');
+
+
+
+
+
+bPane.addEventListener('click', caricaContenuto);
+bPizza.addEventListener('click', caricaContenuto);
+bBiscotti.addEventListener('click', caricaContenuto);
+bBevande.addEventListener('click', caricaContenuto);
+bIngredienti.addEventListener('click', caricaContenuto);
+ricerca.addEventListener('keyup', avviaricerca);
+img1.addEventListener('click', cambiaImg);
+img2.addEventListener('click', cambiaImg);
+img3.addEventListener('click', cambiaImg);
+img5.addEventListener('click', cambiaImg);
